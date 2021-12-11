e82fb26c28c82c53eaaba184c95e1ff92d098266
naah69 <nayan3480232@163.com>
2021-12-11 20:12:40 +0800

    commit

--- js/article.js
+++ js/article.js
@@ -61,62 +61,6 @@ $(document).ready(function () {
     });
 });
 
-
-
-//代码块复制
-var snippets = document.querySelectorAll('td>pre>code');
-var htmlCopyButton = `<button class="codecopy-btn tooltipped tooltipped-sw" aria-label="Copy to clipboard"><i class="fa fa-clipboard" aria-hidden="true"></i></button>`;
-
-snippets.forEach(snippet => {
-
-    var table=snippet.parentNode.parentNode.parentNode.parentNode.parentNode;
-    var div=table.parentNode;
-    var wrapper = document.createElement('div');
-
-    var class0 = (snippet.classList[0] || 'code')
-    if (class0!='code'){
-        var lang = (class0.replace("language-", "") || 'code').toUpperCase();
-        if (lang=="FALLBACK"){
-            lang = "DOC"
-        }
-        wrapper.setAttribute('data-lang', lang);
-        wrapper.style='padding-top:2em;'
-        div.replaceChild(wrapper, table);
-        wrapper.appendChild(table);
-        wrapper.classList.add('code-highlight');
-        wrapper.firstChild.insertAdjacentHTML('beforebegin', htmlCopyButton);
-    }
-
-});
-
-
-// Add copy to clipboard functionality and user feedback
-var clipboard = new ClipboardJS('.codecopy-btn', {
-    target: trigger => {
-        return trigger.nextSibling.firstChild.firstChild.firstChild.lastElementChild;
-    },
-});
-
-clipboard.on('success', e => {
-    e.trigger.setAttribute('aria-label', 'Copied!');
-    e.clearSelection();
-});
-
-// Replace tooltip message when mouse leaves button
-// and prevent page refresh after click button
-var btns = document.querySelectorAll('.codecopy-btn');
-
-btns.forEach(btn => {
-    btn.addEventListener('mouseleave', e => {
-        e.target.setAttribute('aria-label', 'Copy to clipboard');
-        e.target.blur();
-    });
-
-    btn.addEventListener('click', e => {
-        e.preventDefault();
-    });
-});
-
 //图片放大
 $("img").click(function () {
     layer.photos({
@@ -132,3 +76,4 @@ $("img").click(function () {
         }
     });
 });
+
